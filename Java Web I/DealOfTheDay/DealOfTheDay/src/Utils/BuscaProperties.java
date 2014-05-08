1dd0278e7faa57cf8c5309c616b176b6f917da65
Fabio Nascimento <programadornascimento@hotmail.com>
2014-05-08 10:06:17 -0300

    classe que busca arquivo e retorna o properties
    busca pelo caminho do properties
    e retorna o mesmo

---
+++ Java Web I/DealOfTheDay/DealOfTheDay/src/Utils/BuscaProperties.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.comercial.uteis;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ *
+ * @author Administrador
+ */
+public class BuscaProperties {
+
+    private static InputStream is = null;
+
+ //*********************** busca properties em arquivo ********************************
+    public static Properties emArquivo(String caminhoProperties) {
+        Properties temp = new Properties();
+        InputStream inputStream2 = BuscaProperties.class.getClassLoader().getResourceAsStream(caminhoProperties);//"servicos/acessobasico.properties");
+        setIs(inputStream2);
+        try {
+            temp.load( getIs() );
+        } catch (IOException ex) {
+          String mensagem_erro;  mensagem_erro = "Erro ao executar buscarPropertiesBasicasEmArquivo" ;     mensagem_erro += ex.getMessage();
+          System.out.println(mensagem_erro);
+        }
+        return temp;
+    }
+
+    public static void setIs(InputStream is) {
+        BuscaProperties.is = is;
+    }
+
+    public static InputStream getIs() {
+        return is;
+    }
+}
