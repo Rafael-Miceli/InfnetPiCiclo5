11662e383c9c941b17dfcb644652dd4227edb4c0
Fabio Nascimento <programadornascimento@hotmail.com>
2014-05-08 10:27:48 -0300

    implementando properties
    pode testar por favor esse import de properties..
    estou sem ambiente eclipse no trabalho
    e meu not esta no conserto
    se nao puder verificar 
    vou importar e fazer na maquina virtual da facul...
    
    a ideia seria integrar o alex ao projeto no sentido de alimentar nosso banco de dados
    para nao precisarmos ficar criando arrays fixas
    é bem menos complicado que banco de dados
    e talvez seja um plus
    
    se não ficar legal tiramos os properties
    mas seria uma boa vcs saberam manipular eles

--- Java Web I/DealOfTheDay/DealOfTheDay/src/Controller/Controller/Home.java
+++ Java Web I/DealOfTheDay/DealOfTheDay/src/Controller/Controller/Home.java
@@ -7,14 +7,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import uteis.BuscaProperties; //testar caminho do import...estou sem ambiente eclipse aqui no trabalho
+
 public class Home extends HttpServlet{
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub		
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.processRequest(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.processRequest(req, resp);
+	}
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+	{
+		//inicio de teste busca properties..............
+		Properties acessoPropertiesCategoria = new Properties();
+		todasCategoriasDoSistema = new HashMap();
+
+		acessoPropertiesCategoria = BuscaProperties.emArquivo("uteis/categoria.properties");
+		todasCategoriasDoSistema.putAll(acessoPropertiesCategoria);
+		//fim de teste busca properties................
 		
-		req.setAttribute("Welcome", "Conteúdo dinâmico");
+		
+		//inicio de teste setAtribute categoria com o HashMap acima ... para enviar para camada de visao
+		req.setAttribute("todasCategoriasDoSistema", todasCategoriasDoSistema);
+		//fim de teste setAtribute................
+		
+		req.setAttribute("Welcome", "Conteï¿½do dinï¿½mico");
 		req.getRequestDispatcher("Home.jsp").forward(req, resp);
 	}	
+	
+
 }
